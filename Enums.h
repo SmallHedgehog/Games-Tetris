153ce6329d45af6ca017239436d10920239b7ce2
caifei yang <caifei yang>
2017-02-14 16:42:34 +0800

    define enum values

---
+++ Enums.h
@@ -0,0 +1,23 @@
+
+// 定义枚举常量
+
+// 定义游戏块类型
+enum BlockType
+{
+	SQUARE_BLOCK,		// 正方形
+	T_BLOCK,			// T形
+	L_BLOCK,			// L形
+	BACKWARDS_L_BLOCK,	// 反向T形
+	BACKWARDS_T_BLOCK,	// 反向L形
+	STRAIGHT_BLOCK,		// 长方形
+	S_BLOCK,			// S形
+	BACKWARDS_S_BLOCK,	// 反向S形
+};
+
+// 定义反向枚举
+enum Direction
+{
+	LEFT,
+	RIGHT,
+	DOWN
+};
