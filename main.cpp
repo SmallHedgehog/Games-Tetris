4c162efdc435a06e9c20913405075d06fc03ae36
caifei yang <caifei yang>
2017-02-16 21:13:07 +0800

    vs2012 add files

---
+++ main.cpp
@@ -0,0 +1,13 @@
+// main.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "Window.h"
+
+int main(int argc, char* argv[])
+{
+	Window win;
+	win.Start();
+
+	return 0;
+}
