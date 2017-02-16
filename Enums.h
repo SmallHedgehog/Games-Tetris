9419671ce13df165bad99a24f94e6b50e8382368
caifei yang <caifei yang>
2017-02-16 21:06:08 +0800

    define const enum values

--- Enums.h
+++ Enums.h
@@ -7,17 +7,17 @@ enum BlockType
 	SQUARE_BLOCK,		// 正方形
 	T_BLOCK,			// T形
 	L_BLOCK,			// L形
-	BACKWARDS_L_BLOCK,	// 反向T形
 	BACKWARDS_T_BLOCK,	// 反向L形
 	STRAIGHT_BLOCK,		// 长方形
 	S_BLOCK,			// S形
 	BACKWARDS_S_BLOCK,	// 反向S形
 };
 
-// 定义反向枚举
+// 定义方向枚举
 enum Direction
 {
 	LEFT,
 	RIGHT,
-	DOWN
+	DOWN,
+	NONE
 };
